09c58bfea60a611c3ecbe8075c0a4f8db2334f1d
Dervis Cetinalp <dervis.cetinalp@gmail.com>
2019-01-01 17:59:08 -0500

    Adds collectible game objects

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Hit thingy " + other.name);
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
